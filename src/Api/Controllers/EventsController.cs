8f9ab4c3822b1c761487ad20ee0f41410775843b
myllack <randomosoite2222@gmail.com>
2021-05-30 12:02:44 +0300

    Remove unused logger

--- src/Api/Controllers/EventsController.cs
+++ src/Api/Controllers/EventsController.cs
@@ -1,6 +1,5 @@
 ﻿using Application.Events.Queries.GetEvents;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,13 +9,6 @@ namespace Api.Controllers
     [ApiController]
     public class EventsController : ApiControllerBase
     {
-        private readonly ILogger<EventsController> _logger;
-
-        public EventsController(ILogger<EventsController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet("list")]
         public async Task<List<EventViewModel>> Get(CancellationToken cancellationToken)
         {
