8215198a4a8bb99912dbf22c726f7bbfcbb487bb
myllack <randomosoite2222@gmail.com>
2021-05-30 11:16:25 +0300

    Initial commit

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Clean_architecture_demo.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("list")]
+        public IEnumerable<object> Get()
+        {
+            return null;
+        }
+    }
+}
